5cd35b0781bcbc99748ca07281bf737cdd11b5e5
Leonardo Daniel Chipres Arteaga <33239864+lchipres@users.noreply.github.com>
2018-04-27 01:06:51 -0500

    Add files via upload

---
+++ Tortuguini/Tortuguini/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tortuguini
+{
+    public partial class Form1 : Form
+    {
+        Mapa m = new Mapa();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            inicializar(m);
+        }
+
+        private void inicializar(Mapa ms)
+        {
+            int[,] m = new int[20, 20];
+            for(int i = 0; i < 20; i++)
+            {
+                for(int j = 0; j < 20; j++)
+                {
+                    m[i, j] = 0;
+                }
+            }
+            ms.setMapa(m);
+        }
+
+        private string mostrar(Mapa ms)
+        {
+            int[,] m = new int[20, 20];
+            m=ms.getMapa();
+            string s = "";
+            for (int i = 0; i < 20; i++)
+            {
+                s += Environment.NewLine;
+                for (int j = 0; j < 20; j++)
+                {
+                    s += m[i, j];
+                }
+            }
+            return s;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = mostrar(m);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                m.avanzar(Convert.ToInt32(textBox2.Text));
+            }
+            catch (IndexOutOfRangeException est)
+            {
+                throw est;
+            }
+}
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            m.girarDerecha();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            m.girarIzquierda();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            m.bajarPluma();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            m.subirPluma();
+        }
+    }
+}
---
+++ Tortuguini/Tortuguini/Mapa.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tortuguini
+{
+    class Mapa
+    {
+        int[,] mapa = new int[20, 20];
+        int pluma = 0, direccion = 3,x=0,y=0;
+        //Direcciones 3=arriba 4=derecha 5=abajo 6=izquierda
+
+        public void setMapa(int[,] m)
+        {
+            mapa = m;
+        }
+
+        public int[,] getMapa()
+        {
+            return mapa;
+        }
+
+        public void bajarPluma()
+        {
+            pluma = 1;
+        }
+
+        public void subirPluma()
+        {
+            pluma = 0;
+        }
+
+        public void girarDerecha()
+        {
+            if(direccion==6)
+            {
+                direccion = 3;
+            }
+            else
+            {
+                direccion+=1;
+            }
+        }
+
+        public void girarIzquierda()
+        {
+            if (direccion == 3)
+            {
+                direccion = 6;
+            }
+            else
+            {
+                direccion-=1;
+            }
+        }
+
+        public void avanzar(int n)
+        {
+           
+                if (direccion == 3)
+                {
+                    for (; n > 0 && x < 20;)
+                    {
+                        if (pluma == 1)
+                        {
+                        mapa[x, y] = 1;
+                        }
+                        x -= 1;
+                        n -= 1;
+                    }
+                }
+
+                if (direccion == 4)
+                {
+                    for (; n > 0 && y < 20;)
+                    {
+                        if (pluma == 1)
+                        {
+                        mapa[x, y] = 1;
+                        }
+                        y += 1;
+                        n -= 1;
+                    }
+                }
+
+                if (direccion == 5)
+                {
+                    for (; n >0&&x<20;)
+                    {
+                        if (pluma == 1)
+                        {
+                            mapa[x, y] = 1;
+                        }
+                        x+=1;
+                        n-=1;
+                    }
+                }
+
+                if (direccion == 6)
+                {
+                    for (; n > 0 && y < 20;)
+                    {
+                        if (pluma == 1)
+                        {
+                        mapa[x, y] = 1;
+                        }
+                        y -= 1;
+                        n -= 1;
+                    }
+                }
+
+        }
+    }
+}
+
